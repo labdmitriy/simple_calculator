31017cef419f00a41ed2db548d3e35ec55c771b0
Dmitry Labazkin <labdmitriy@gmail.com>
2023-02-22 16:54:12 +0300

    Complete steps 1-5

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+
+from functools import reduce
+
+
+class SimpleCalculator:
+    def add(self, *args):
+        return sum(args)
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, *args):
+        return reduce(lambda x, y: x * y, args)
